854ddf9e373c3b761fc3206848a95b5c187a93cb
Matteo Manuelli <manuelli.matteo01@gmail.com>
2022-07-21 15:21:13 +0200

    Update downloadPosts.py

--- downloadPosts.py
+++ downloadPosts.py
@@ -5,26 +5,104 @@ import threading
 import sys
 import time
 
-def printTime():
+def printGetTime():
     currentTime = time.strftime("%H:%M:%S", time.localtime())
     print(currentTime)
     return currentTime
+
+def download(followers):
+
+    global pubblic
+    global private
+
+    ig = IG()
+    pubb, priv = ig.downloadProfiles(followers)
+
+    printStatMutex.acquire()
+
+    pubblic +=pubb
+    private +=priv
+
+    # consistency of data
+    print(f"\n\n{pubblic}/{totFollowers} pubblic profiles")
+    print(f"{private}/{totFollowers} private profiles\n\n")
+    print(f"{pubblic + private}/{totFollowers} parsed")
+
+    printStatMutex.release()
+
+
+def getFollowers(username):
+    #returns followers list
+    #temporary instaloader instance to get followers. Credentials needed
+    #only here
+    myUsername = sys.argv[1] 
+    myPassword = sys.argv[2]
+
+    temp = instaloader.Instaloader()
+    temp.login(myUsername, myPassword)
+
+    # '<Profile noegurieri (5677279416)>'
+    followers = [str(follower).split()[1] for follower in instaloader.Profile.from_username(temp.context, username).get_followers()]
+
+    temp.close()
+    return followers
+
+startTime = printGetTime()
+
+MAX_PER_THREAD = 30
+MAX_THREADS = 3
+
+pubblic = 0
+private = 0
+printStatMutex = threading.Lock()
+
+#global variables
+followers = getFollowers(sys.argv[3]) #read only
+totFollowers = len(followers) #read only
+mutex = threading.Lock()
+i = 0 
+hasFollowersToParse = True
+
 
 class IG:
     def __init__(self):
         #quiet=True
         self.L = instaloader.Instaloader(
-                            save_metadata=False,
-                            download_pictures=True,
-                            download_videos=False,
-                            download_video_thumbnails=True,
-                            download_comments=False,
-                            download_geotags=False,
-                            post_metadata_txt_pattern="")
- 
+                save_metadata=False,
+                download_pictures=True,
+                download_videos=False,
+                download_video_thumbnails=True,
+                download_comments=False,
+                download_geotags=False,
+                post_metadata_txt_pattern="")
+
+    def getNextFollowers(self):
+        global i
+        global hasFollowersToParse
+        global mutex
+        #global followers
+        #global totFollowers
+        #global MAX_PER_THREAD
+        #global MAX_THREADS
+
+        mutex.acquire()
+        
+        if hasFollowersToParse == False:
+            mutex.release()
+            return False
+
+        followersToReturn = followers[i:min(i+MAX_PER_THREAD, totFollowers)]
+        if i + MAX_PER_THREAD >= totFollowers: hasFollowersToParse = False
+        else: i+=MAX_PER_THREAD
+
+        mutex.release()
+
+        return followersToReturn
+
+
     def downloadPosts(self, profile, resume=False):
-    #Download posts of a profile. returns 0 on success, 1 else
-    
+        #Download posts of a profile. returns 0 on success, 1 else
+
         count = 0
         posts = profile.get_posts()
 
@@ -39,10 +117,10 @@ class IG:
 
         # except instaloader.exceptions.LoginRequiredException: 
         except: print(f"\nEXCEPTION {profile.username}: {count}\n")
-   
-    def downloadProfiles(self, followers, close=False):
-    #download n profiles
-        
+
+    def downloadProfiles(self, followers):
+        #download n profiles
+
         threads = []
         pubblic = 0
         private = 0
@@ -66,77 +144,38 @@ class IG:
         # for thread in threads: thread.start()
         for thread in threads: thread.join()
 
-        if close == True: self.L.close()
-        return pubblic, private
+        nextFollowers = self.getNextFollowers()
+        if nextFollowers == False:
+            self.L.close()
+            return pubblic, private
 
-def getFollowers(username):
-    #returns followers list
-    #temporary instaloader instance to get followers. Credentials needed
-    #only here
-    myUsername = sys.argv[1] 
-    myPassword = sys.argv[2] 
-
-    temp = instaloader.Instaloader()
-    temp.login(myUsername, myPassword)
-   
-    # '<Profile noegurieri (5677279416)>'
-    followers = [str(follower).split()[1] for follower in instaloader.Profile.from_username(temp.context, username).get_followers()]
-
-    temp.close()
-    return followers
+        else:
+            pubb, priv = self.downloadProfiles(nextFollowers) #recursion
+            return pubblic + pubb, private + priv
 
 
-startTime = printTime()
 
-followers = getFollowers(sys.argv[3])
-totFollowers = len(followers)
-MAX_PER_THREAD = 30
-MAX_THREADS = 3
-pubblic = 0
-private = 0
-mutex = threading.Lock()
-i = 0
-flag = True
 
-def download(followers, batchFinished=False):
 
-    global pubblic
-    global private
 
-    ig = IG()
-    pubb, priv = ig.downloadProfiles(followers)
+
+
+threads = []
+j = 0
+while j < MAX_THREADS and hasFollowersToParse: #init phase
 
     mutex.acquire()
 
-    pubblic +=pubb
-    private +=priv
-   
-    # consistency of data
-    print(f"\n\n{pubblic}/{totFollowers} pubblic profiles")
-    print(f"{private}/{totFollowers} private profiles\n\n")
-    print(f"{pubblic + private}/{totFollowers} parsed")
+    thread = threading.Thread(target = download, args= [followers[i:min(i+MAX_PER_THREAD, totFollowers)]])
+    threads.append(thread)
+    thread.start()
+    j+=1
+    if i + MAX_PER_THREAD >= totFollowers: hasFollowersToParse = False
+    else: i+=MAX_PER_THREAD
 
     mutex.release()
 
+for thread in threads: thread.join()
 
-
-
-while i < totFollowers and flag:
-    
-    #reset
-    j = 0
-    threads = []
-
-    while j < MAX_THREADS and flag: #init phase
-        
-        thread = threading.Thread(target = download, args= [followers[i:min(i+MAX_PER_THREAD, totFollowers)]])
-        threads.append(thread)
-        thread.start()
-        j+=1
-        if i + MAX_PER_THREAD >= totFollowers: flag = False
-        else: i+=MAX_PER_THREAD
-
-    for thread in threads: thread.join()
-
-finishTime = printTime()
+finishTime = printGetTime()
 print(startTime, finishTime)
